0ff0325ad98cd500ba6f281ca198dca20afdfdfc
mrunal mule <mrunal.mule28@gmail.com>
2018-03-19 14:51:55 +0530

    Initial Commit

--- app/src/main/java/com/example/mrunal/mentor365/MainActivity.java
+++ app/src/main/java/com/example/mrunal/mentor365/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.mrunal.mentor365;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+*/
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, FirstScreen.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
+
