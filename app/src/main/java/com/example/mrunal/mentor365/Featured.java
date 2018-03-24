55ce5c57ed749ee911021c1f8ca564247e812a9a
Shree <akolkar.akhi@gmail.com>
2018-03-24 11:45:36 +0530

    video chat added in navigation drawer.

--- app/src/main/java/com/example/mrunal/mentor365/Featured.java
+++ app/src/main/java/com/example/mrunal/mentor365/Featured.java
@@ -1,5 +1,6 @@
 package com.example.mrunal.mentor365;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,6 +81,7 @@ public class Featured extends AppCompatActivity
         if (id == R.id.features) {
 
         } else if (id == R.id.mymentors) {
+            startActivity(new Intent(Featured.this, videochat.class));
 
         } else if (id == R.id.topmentors) {
 
